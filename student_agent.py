d123c89f5fe871cce175e04615d0d5cf463c2276
b10902111 <b10902111@meow1.csie.ntu.edu.tw>
2025-03-24 11:22:36 +0800

    new weights

--- student_agent.py
+++ student_agent.py
@@ -5,7 +5,11 @@ from TaxiMemory import TaxiMemory
 from utils import get_state_tensor
 from dqn_net import DQN
 
-device = "cpu"
+device = torch.device(
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available() else "cpu"
+)
 
 
 def interactive():
